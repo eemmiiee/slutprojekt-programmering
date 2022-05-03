9983c502d2df01f54cad17d5891d5abebc1726a3
emmie <emmievolcsey@yahoo.com>
2022-05-03 16:19:19 +0200

    skapade flight

--- emmie flappybird/Assets/Scripts/Flight.cs
+++ emmie flappybird/Assets/Scripts/Flight.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallMover : MonoBehaviour
+public class Flight : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class BallMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(1,0,0) * Time.deltaTime;
+        
     }
 }
